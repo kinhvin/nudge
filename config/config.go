4a68177a5eb1f4abb7999c9c91ec54da93de96c2
Kevin Truong <kevintruong.code@gmail.com>
2026-02-23 18:20:37 -0500

    Revert "Fix Actions Workflow"

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ type Config struct {
 func Load() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("No .env file found, reading from environment variables")
+		log.Fatal("Error loading .env file")
 	}
 
 	return &Config{
